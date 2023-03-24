61f7077f9a3ed997b30fe3819656c7017e6177de
Tory <88531407+1two13@users.noreply.github.com>
2023-03-24 15:34:54 +0900

    refactor: 프록시 주소 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,12 +3,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(['/api'], {
-      target: 'http://ec2-13-125-255-195.ap-northeast-2.compute.amazonaws.com:8068',
+      target: 'https://ec2-3-39-238-222.ap-northeast-2.compute.amazonaws.com:18068',
       changeOrigin: true,
       onProxyReq: function (proxyReq) {
         proxyReq.setHeader(
           'origin',
-          'http://ec2-13-125-255-195.ap-northeast-2.compute.amazonaws.com:8068'
+          'https://ec2-3-39-238-222.ap-northeast-2.compute.amazonaws.com:18068'
         );
       },
     })
