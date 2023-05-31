3cb8015cf6ee8acaacb8a2184c64317f2698fcb0
Tory <88531407+1two13@users.noreply.github.com>
2023-05-31 19:49:10 +0900

    style: css 수정

--- src/components/main/SearchInput.tsx
+++ src/components/main/SearchInput.tsx
@@ -2,7 +2,7 @@ import { SEARCH_PLACEHOLDER } from '../../static/constants';
 
 function SearchInput() {
   return (
-    <div className="w-[25%] h-[60%] ml-[50%] rounded-sm">
+    <div className="w-[100%] h-[60%] ml-[30%] rounded-sm">
       <input
         placeholder={SEARCH_PLACEHOLDER}
         className="w-[100%] h-[100%] pl-[3%] bg-lightGray/20 focus:outline-none"
