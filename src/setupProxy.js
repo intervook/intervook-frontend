7725470e726d8f62b0dcce459186f4b1b2e7c398
Tory <88531407+1two13@users.noreply.github.com>
2023-05-31 20:37:54 +0900

    refactor: 서버 주소 변경

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,12 +3,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(['/api'], {
-      target: 'https://ec2-3-39-238-222.ap-northeast-2.compute.amazonaws.com:18068',
+      target: 'http://ec2-43-201-65-255.ap-northeast-2.compute.amazonaws.com:18068/',
+      // target: 'http://192.168.0.21:18068',
       changeOrigin: true,
       onProxyReq: function (proxyReq) {
         proxyReq.setHeader(
           'origin',
-          'https://ec2-3-39-238-222.ap-northeast-2.compute.amazonaws.com:18068'
+          'http://ec2-43-201-65-255.ap-northeast-2.compute.amazonaws.com:18068/'
+          // 'http://192.168.0.21:18068'
         );
       },
     })
